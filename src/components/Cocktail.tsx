deef86df97af8b0eacf75ee7288684eeff38df89
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-08-04 19:22:25 -0300

    display list

--- src/components/Cocktail.tsx
+++ src/components/Cocktail.tsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
 
-export function Cocktail() {
+import { CocktailData } from "../@types/cocktail";
+
+export function Cocktail({ id, name, image, info, glass }: CocktailData) {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
+          details
+        </Link>
+      </div>
+    </article>
   );
 }
